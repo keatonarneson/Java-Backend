264b7c8050dc254ce7f26eb1355413361704c072
Keaton <keaton.arneson@gmail.com>
2021-08-17 20:37:19 -0500

    Added WAR logic and functionality with final results

--- Week6/src/App.java
+++ Week6/src/App.java
@@ -1,5 +1,50 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Deck deck = new Deck();
+        Player player1 = new Player("Player1");
+        Player player2 = new Player("Player2");
+        deck.shuffle();
+
+        // Populating players' hands
+        for (int i = 0; i < 52; i++) {
+            if (i % 2 == 0) {
+                player1.draw(deck);
+            } else {
+                player2.draw(deck);
+            }
+        }
+
+        System.out.println("-----PLAYERS-----");
+        player1.describe();
+        player2.describe();
+
+        // Playing WAR
+        for (int i = 0; i < 26; i++) {
+            int player1CardValue = player1.flip().getValue();
+            int player2CardValue = player2.flip().getValue();
+
+            if (player1CardValue < player2CardValue) {
+                player2.incrementScore();
+            } else if (player1CardValue > player2CardValue) {
+                player1.incrementScore();
+            }
+        }
+
+        // Comparing players' scores
+        int player1Score = player1.getFinalScore();
+        int player2Score = player2.getFinalScore();
+
+        System.out.println("-----FINAL SCORE-----");
+        System.out.println("Player1 Score: " + player1Score);
+        System.out.println("Player2 Score: " + player2Score);
+
+        if (player1Score > player2Score) {
+            System.out.println("Player 1 Wins!");
+        } else if (player1Score < player2Score) {
+            System.out.println("Player 2 Wins!");
+        } else {
+            System.out.println("It is a Draw!");
+        }
+
     }
 }
